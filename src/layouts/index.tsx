8bd3c15d8436be98619d2bbe8b161349712f0484
Pierre Maclot <pierremaclot@MacBook-Pro-de-Pierre.local>
2023-08-24 10:49:50 +0200

    Using new CSS viewport units

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -13,7 +13,18 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => (
-  <Box sx={{ height: '100vh', width: '100vw' }}>
+  <Box
+    sx={{
+      height: '100vh',
+      // @ts-ignore
+      // eslint-disable-next-line no-dupe-keys
+      height: '100svh',
+      width: '100vw',
+      // @ts-ignore
+      // eslint-disable-next-line no-dupe-keys
+      width: '100svw'
+    }}
+  >
     <Header sx={{ position: 'fixed', top: 0, width: '100%', zIndex: 200 }} />
     <Box sx={{ height: '100%', width: '100%', zIndex: 0 }}>{children}</Box>
     <Footer sx={{ position: 'fixed', bottom: 0, width: '100%', zIndex: 200 }} />
