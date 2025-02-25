f622944fff5c9bf8d1367bdeedaab65058cec565
pmaclot <36260986+pmaclot@users.noreply.github.com>
2025-02-25 17:52:01 +0100

    Room v2 wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,48 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useRef } from 'react';
 
 // Components
-import Room from '../components/room';
+import Room from '../components/room2';
 
 // Externals
-import { BakeShadows, CameraControls, Html, Preload, Stage, useProgress } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import { BakeShadows, CameraControls, Html, Preload, Stage, useHelper, useProgress } from '@react-three/drei';
+
 import CameraControlsImpl from 'camera-controls';
 import type { HeadFC, PageProps } from 'gatsby';
 
 // Layouts
 import Layout from '../layouts';
+import { DirectionalLight, DirectionalLightHelper } from 'three';
 
 const IndexPage: React.FC<PageProps> = () => {
+  const dirLight = useRef<DirectionalLight>(null);
+
   return (
     <Layout>
       <Canvas
         camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 10] }}
-        dpr={[1, 2]}
         frameloop="demand"
         orthographic={true}
         shadows={true}
       >
         <Suspense fallback={<Loader />}>
-          <Stage adjustCamera={false} environment="forest" intensity={1} shadows="contact">
+          {/* <ambientLight intensity={1} /> */}
+          <directionalLight
+            castShadow={true}
+            color={0xffffff}
+            intensity={7}
+            position={[-5, 5, -10]}
+            ref={dirLight}
+            rotation={[-2, -1, 0]}
+          />
+          {/* {dirLight.current && <directionalLightHelper args={[dirLight.current, 4, 0xff0000]} />}  */}
+          <Stage adjustCamera={false} environment="forest" intensity={0.2} shadows="contact">
             <Room />
           </Stage>
 
           <Preload all={true} />
 
-          <BakeShadows />
+          {/* <BakeShadows /> */}
 
           <CameraControls
             enabled={true}
@@ -60,7 +73,7 @@ const IndexPage: React.FC<PageProps> = () => {
 function Loader() {
   const { progress } = useProgress();
 
-  return <Html center={true}>{progress}% loaded</Html>;
+  return <Html center={true}>{progress.toFixed(0)}% loaded</Html>;
 }
 
 export default IndexPage;
